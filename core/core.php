5a487772b5de7d2c89f618ce8b4272643a1ef8b6
FabienMeunier <meunier.fabien@yahoo.fr>
2020-04-07 16:53:45 +0200

    initial commit

---
+++ core/core.php
@@ -0,0 +1,20 @@
+<?php
+
+/* 
+ * AUTEUR: Fabien Meunier
+ * PROJECT: Third_Type_Tapes
+ * PATH: Third_Type_Tapes/core/
+ * NAME: core.php
+ */
+
+session_start();
+try{
+    $db = new PDO("mysql:host=mysql.1;port=3306;dbname=thirdtypetapes_c", "thirdtypetapes_c", "7qq3jL*5", array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+    $db->exec('SET sql_mode=""');
+}
+catch(Exception $e){
+    die('Error : '.$e->getMessage());
+}
+
+require("core/model.php");
+
